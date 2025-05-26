c862d77b1f0ef4c16393104a0d505fbeb19dfc69
AdnanBahaaeldin <167567127+AdnanBahaaeldin@users.noreply.github.com>
2025-05-27 00:02:53 +0300

    Added Main Program

---
+++ Main Program/API/Models/GroupMember.cs
@@ -0,0 +1,11 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Models;
+
+public class GroupMember
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+   
+}
